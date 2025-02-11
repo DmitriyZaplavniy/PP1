919b402727a13e1a37357252e807ce7960a07e56
dima <zik871@outlook.com>
2025-02-11 21:08:58 +0300

    исправления

--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,13 @@ import os
 
 
 def read_json_file(file_path):
+    """Читает JSON-файл и возвращает список словарей с данными о финансовых транзакциях."""
     if not os.path.exists(file_path):
         return []
 
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, 'r', encoding='utf-8') as f:
         try:
-            data = json.load(file)
+            data = json.load(f)
             if isinstance(data, list):
                 return data
             else:
